1903ccaf4560984944810c2c84019369ef4a6771
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2023-12-12 12:45:05 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -14,7 +14,7 @@ class mr(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├🤖 My Name is : {}
-├👑 CHANNEL : <a href=https://t.me/KingKc18>VJ Bots</a> 
+├👑 ADMIN : <a href=https://t.me/KingKc18>Max46 Kc</a> 
 ├📕 LIBRARY : <a href=https://github.com/pyrogram>Pyrogram</a>
 ├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
 ├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>
